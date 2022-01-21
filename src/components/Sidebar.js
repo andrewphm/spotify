6a8809a2727497fc4d155564b36abe066fb26e91
andrewphm <andrpham@gmail.com>
2022-01-21 12:38:30 -0500

    Create middleware to redirect to login page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,12 +6,22 @@ import {
   HeartIcon,
   RssIcon,
 } from '@heroicons/react/outline';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 const Sidebar = () => {
+  const { data: session, status } = useSession();
+
+  console.log(session);
+
   return (
     <div className="text-gray-500 m-4 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
+        <button
+          onClick={() => signOut()}
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <p>Log out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon onClick={() => signOut()} className="h-5 w-5" />
           <p>Home</p>
